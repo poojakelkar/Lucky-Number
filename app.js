6282bb20019f1bb877f9914ecc7a2c09b6dd3123
Pooja Kelkar <poojakelkar53@gmail.com>
2021-09-20 21:50:35 +0530

    birthday lucky or not chcek

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("lucky-number");
 const checkNumberButton = document.querySelector("#check");
 
+function checkValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        console.log("Your Birthday is lucky");
+    } else {
+        console.log("Your Birthday is not lucky");
+    }
+}
+
 function checkNumberIsLucky() {
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    console.log(sum);
+    checkValues(sum, luckyNumber.values);
 }
 
 function calculateSum(dob) {
